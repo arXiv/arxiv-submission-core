207de4b01e175284c2c15f71498ee4b1126d1acc
erickpeirson <brp53@cornell.edu>
2018-04-05 16:22:41 -0400

    rebuild gh pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,8 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx.ext.graphviz',
-    'sphinx.ext.githubpages'
+    'sphinx.ext.githubpages',
+    'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
