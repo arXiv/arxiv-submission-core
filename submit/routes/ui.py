7de3dfd39cc924645b5bd59a6cd9bd215aa39edd
eaw82 <eaw82@cornell.edu>
2018-05-24 16:44:41 -0400

    ARXIVNG-859 add first three templates

--- submit/routes/ui.py
+++ submit/routes/ui.py
@@ -6,10 +6,82 @@ from arxiv import status
 
 blueprint = Blueprint('ui', __name__, url_prefix='/')
 
+# TODO: might be refactored into a series of macros and fewer single-page
+# templates, initial setup is for testing purposes and to get something started.
 
-@blueprint.route('verify-user', methods=['GET'])
+@blueprint.route('/', methods=['GET'])
 def verify_user():
     """Render the submission start page. Foreshortened validation for template testing."""
     rendered = render_template("submit/verify_user.html", pagetitle='Verify User Information')
+    response = make_response(rendered, status.HTTP_200_OK)
+    return response
+
+@blueprint.route('authorship', methods=['GET'])
+def authorship():
+    """Render step 2, authorship. Foreshortened validation for template testing."""
+    rendered = render_template("submit/authorship.html", pagetitle='Confirm Authorship')
+    response = make_response(rendered, status.HTTP_200_OK)
+    return response
+
+@blueprint.route('license', methods=['GET'])
+def license():
+    """Render step 3, license selection. Foreshortened validation for template testing."""
+    rendered = render_template("submit/license.html", pagetitle='Select a License')
+    response = make_response(rendered, status.HTTP_200_OK)
+    return response
+
+@blueprint.route('policy_ack', methods=['GET'])
+def policy_ack():
+    """Render step 4, policy agreement. Foreshortened validation for template testing."""
+    rendered = render_template("submit/policy.html", pagetitle='Acknowledge Policy Statement')
+    response = make_response(rendered, status.HTTP_200_OK)
+    return response
+
+@blueprint.route('classification', methods=['GET'])
+def classification():
+    """Render step 5, choose primary class. Foreshortened validation for template testing."""
+    rendered = render_template("submit/classification.html", pagetitle='Choose a Primary Classification')
+    response = make_response(rendered, status.HTTP_200_OK)
+    return response
+
+@blueprint.route('cross', methods=['GET'])
+def secondary_class():
+    """Render step 6, secondary classes. Foreshortened validation for template testing."""
+    rendered = render_template("submit/secondary_class.html", pagetitle='Choose Secondary Classifications')
+    response = make_response(rendered, status.HTTP_200_OK)
+    return response
+
+@blueprint.route('file_upload', methods=['GET'])
+def file_upload():
+    """Render step 7, file add or edit. Foreshortened validation for template testing."""
+    rendered = render_template("submit/file_upload.html", pagetitle='Add or Edit Files')
+    response = make_response(rendered, status.HTTP_200_OK)
+    return response
+
+@blueprint.route('file_process', methods=['GET'])
+def file_process():
+    """Render step 8, file processing. Foreshortened validation for template testing."""
+    rendered = render_template("submit/file_process.html", pagetitle='Process Files')
+    response = make_response(rendered, status.HTTP_200_OK)
+    return response
+
+@blueprint.route('add_metadata', methods=['GET'])
+def add_metadata():
+    """Render step 9, metadata. Foreshortened validation for template testing."""
+    rendered = render_template("submit/add_metadata.html", pagetitle='Add or Edit Metadata')
+    response = make_response(rendered, status.HTTP_200_OK)
+    return response
+
+@blueprint.route('final_preview', methods=['GET'])
+def final_preview():
+    """Render step 10, preview. Foreshortened validation for template testing."""
+    rendered = render_template("submit/final_preview.html", pagetitle='Preview and Approve')
+    response = make_response(rendered, status.HTTP_200_OK)
+    return response
+
+@blueprint.route('confirm_submit', methods=['GET'])
+def confirm_submit():
+    """Render the final confirmation page. Foreshortened validation for template testing."""
+    rendered = render_template("submit/confirm_submit.html", pagetitle='Submission Confirmed')
     response = make_response(rendered, status.HTTP_200_OK)
     return response
