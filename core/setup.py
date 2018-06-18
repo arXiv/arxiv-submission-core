0816341f3affade13ac5daeabb79d80d94e1eb7e
erickpeirson <brp53@cornell.edu>
2018-06-18 10:17:23 -0400

    incremented patch version

--- core/setup.py
+++ core/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-submission-events',
-    version='0.2.2',
+    version='0.2.3',
     packages=find_packages(exclude=['test*']),
     zip_safe=False
 )
