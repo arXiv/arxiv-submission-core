0bb081056bfa6dadfaad56cab8208e08cad740ef
erickpeirson <brp53@cornell.edu>
2018-06-21 16:04:03 -0400

    incremented minor version to 0.3.1

--- core/setup.py
+++ core/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-submission-events',
-    version='0.2.4',
+    version='0.3.1',
     packages=find_packages(exclude=['test*']),
     zip_safe=False
 )
